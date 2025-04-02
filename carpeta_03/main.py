818dcaeaa1d9d31a879b1e213d04673c234b4199
Valeria Ascanio <valascanio08@gmail.com>
2025-04-02 08:28:16 -0500

    main.py

--- carpeta_03/main.py
+++ carpeta_03/main.py
@@ -1,7 +1,7 @@
 # Estudiante: corrige este código y haz un pull request con la versión corregida.
 
 class Persona:
-    def __init__(self, nombre)
+    def __init__(self, nombre):
         self.nombre = nombre
 
 p = Persona('Ana')
